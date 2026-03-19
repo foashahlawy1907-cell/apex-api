3a0145e7fd95a82e457d774c346ae6b55c9d6b7f
foashahlawy1907-cell <foashahlawy1907@gmail.com>
2026-03-19 10:05:38 +0200

    Update st.js

--- api/st.js
+++ api/st.js
@@ -1,19 +1,19 @@
 module.exports = (req, res) => {
-  // سحب رابط الفصل من الـ Query
+  // سحب رابط الفصل الذي يرسله موقع Dilar
   const urlParam = req.query.url;
   
-  // رابط صفحتك الجديدة في بلوجر
-  const bloggerPage = "https://apex-team1.blogspot.com/2026/03/apex-team-redirect-system-body-font.html";
+  // الرابط الجديد الذي زودتني به (بدون m=1 لضمان عمل الإعلانات بكفاءة)
+  const bloggerPage = "https://apex-team1.blogspot.com/2026/03/apex-team-secure-redirect-body-font.html";
 
   if (!urlParam) {
-    // لو مفيش رابط فصل، ارجعه لصفحة بلوجر الرئيسية
+    // لو مفيش رابط فصل، ارجعه لصفحة بلوجر مباشرة
     return res.redirect(bloggerPage);
   }
 
-  // بناء الرابط النهائي (تأكد من استخدام encodeURIComponent)
+  // بناء الرابط النهائي مع تشفير رابط الوجهة
   const finalRedirect = `${bloggerPage}?url=${encodeURIComponent(urlParam)}`;
 
-  // الرد بصيغة JSON التي يقرأها Dilar ليقوم بالتحويل
+  // الرد بصيغة JSON التي يتوقعها Dilar للتحويل التلقائي
   res.setHeader('Content-Type', 'application/json');
   res.status(200).send(JSON.stringify({
     status: "success",
